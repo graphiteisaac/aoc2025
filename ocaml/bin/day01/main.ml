eade1768a32db675c308b28611b7f5c516011b99
graphiteisaac <isaac@grphcrtv.com>
2025-12-01 21:47:22 +1100

    (ocaml) day 1 part 2

--- ocaml/bin/day01/main.ml
+++ ocaml/bin/day01/main.ml
@@ -28,7 +28,41 @@ let part_one (input : rotation list) =
   in
   loop (0, 50) input
 
-let part_two _input = 0
+let part_two (input : rotation list) =
+  let rec loop (count, current) = function
+    | [] -> count
+    | rotation :: rest -> (
+        match rotation with
+        | Left degrees ->
+            let updated = current - degrees in
+            let new_position = updated mod 100 in
+            let num_zeroes =
+              let updated_f = float updated /. 100.0 in
+              match current with
+              | 0 -> int_of_float (ceil updated_f) |> ( ~- )
+              | _ -> int_of_float (floor updated_f) |> ( ~- )
+            in
+            let next_count, next_pos =
+              if new_position > 0 then
+                (count + num_zeroes, new_position)
+              else if new_position = 0 then
+                (count + num_zeroes + 1, new_position)
+              else
+                (count + num_zeroes, 100 + new_position)
+            in
+            loop (next_count, next_pos) rest
+        | Right degrees ->
+            let updated = current + degrees in
+            let num_zeroes =
+              let updated_f = float updated /. 100.0 in
+              int_of_float (floor updated_f)
+            in
+
+            let next_count = count + num_zeroes in
+            let next_pos = updated mod 100 in
+            loop (next_count, next_pos) rest)
+  in
+  loop (0, 50) input
 
 let parse input =
   List.map
